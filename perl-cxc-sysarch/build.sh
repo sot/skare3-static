bd74c83f12effd8dd06b2b16441ee60ac05f40cd
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-02 16:31:52 -0400

    Add CXC::SysArch

---
+++ perl-cxc-sysarch/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+chmod +x ${RECIPE_DIR}/sysarch
+chmod +x ${RECIPE_DIR}/syspathsubst
+cp -a ${RECIPE_DIR}/sysarch ${PREFIX}/bin
+cp -a ${RECIPE_DIR}/syspathsubst ${PREFIX}/bin
+
+
+# If it has Build.PL use that, otherwise use Makefile.PL
+if [ -f Build.PL ]; then
+    perl Build.PL
+    ./Build
+    ./Build test
+    # Make sure this goes in site
+    ./Build install --installdirs site
+elif [ -f Makefile.PL ]; then
+    # Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
+    make install
+else
+    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
+    exit 1
+fi
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
