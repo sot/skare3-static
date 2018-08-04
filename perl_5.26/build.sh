57e4f504766fa3e74142a5c71cb1a33ca2d5d7ad
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-04 18:51:19 -0400

    Remove extra space in otherlibdirs

--- perl_5.26/build.sh
+++ perl_5.26/build.sh
@@ -9,7 +9,7 @@
             -Dinstallsiteman3dir="${PREFIX}/share/man/man3" \
             -Dsiteman1dir="${PREFIX}/share/man/man1" \
             -Dsiteman3dir="${PREFIX}/share/man/man3" \
-            -Dotherlibdirs="${PREFIX}/lib/perl " \
+            -Dotherlibdirs="${PREFIX}/lib/perl" \
             -A prepend:ldflags=" -headerpad_max_install_names -L${PREFIX}/lib " \
 	    -A prepend:lddlflags=" -headerpad_max_install_names -L${PREFIX}/lib " \
             -A prepend:cflags=" -I${PREFIX}/include " \
