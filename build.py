1d3d3acb0e9c67f68bc21872b2bbafead334729c
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-04 07:55:04 -0400

    Script improvements

--- build.py
+++ build.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -E
 
 import os
 import subprocess
+import argparse
 
-pkgs = ['cfitsio',
+parser = argparse.ArgumentParser(description="Build skare3 Perl packages.")
+
+parser.add_argument("package", type=str, nargs="?",
+                    help="Package to build.  All updated packages will be built if no package supplied")
+parser.add_argument("--build-root", default=".", type=str,
+                    help="Path to root directory for output conda build packages."
+                         "Default: '.'")
+args = parser.parse_args()
+
+all_pkgs = ['cfitsio',
 	'pgplot',
 	'xtime',
 	'perl',
@@ -21,8 +31,17 @@ if os.uname().sysname == 'Darwin':
    # Not sure if this is really being used by the compiler, but ...
    os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
    # Remove PGPLOT on OSX
-   pkgs.remove('pgplot')
-   pkgs.remove('perl-pgplot')
+   all_pkgs.remove('pgplot')
+   all_pkgs.remove('perl-pgplot')
 
+build_dir = os.path.join(args.build_root, "builds")
+
+# If a package is requested, do that, else everything.
+if args.package is None:
+   pkgs = all_pkgs
+else:
+   pkgs = [args.package]
+   
 for pkg in pkgs:
-   subprocess.check_call(["conda", "build", "--old-build-string", "--skip-existing", "--perl", "5.18.2", pkg])
+   subprocess.check_call(["conda", "build", "--old-build-string", "--skip-existing",
+                          "--perl", "5.18.2", "--croot", build_dir, pkg])
