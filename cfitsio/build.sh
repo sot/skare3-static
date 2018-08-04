64af7eb92ffe0abe048a271f596043a351bb9148
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-04 15:12:14 -0400

    Update builds for macos (need edit for linux)

--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export CFLAGS=" -Wl,-headerpad_max_install_names "
+export LDFLAGS=" -headerpad_max_install_names "
 ./configure --prefix=$PREFIX
 make
 make install
--- xtime/build.sh
+++ xtime/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-./configure --prefix ${PREFIX}
+export CFLAGS=" -Wl,-headerpad_max_install_names "
+export LDFLAGS=" -headerpad_max_install_names "
+./configure --prefix=$PREFIX
 make
 make install
