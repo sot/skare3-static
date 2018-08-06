be8134c69913a877d56d9298f1c28a2e84242700
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-06 11:33:23 -0400

    Add build flags for OSX

--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-export CFLAGS=" -Wl,-headerpad_max_install_names "
-export LDFLAGS=" -headerpad_max_install_names "
+if [ `uname` == Darwin ]; then
+    export CFLAGS=" -Wl,-headerpad_max_install_names "
+    export LDFLAGS=" -headerpad_max_install_names "
+fi
+
 ./configure --prefix=$PREFIX
 make
 make install
