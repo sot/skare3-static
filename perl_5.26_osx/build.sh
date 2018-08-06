e407b23b576ff2a77fcaff8e890049c57d0d1d72
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-08-06 09:55:57 -0400

    Remove otherlibdirs and '.' in @INC from mac perl config

--- perl_5.26_osx/build.sh
+++ perl_5.26_osx/build.sh
@@ -2,14 +2,12 @@
 ./Configure -des -Dprefix=${PREFIX} \
             -Duserelocatableinc \
             -Dusesitecustomize \
-	    -Udefault_inc_excludes_dot \
             -Dman1dir="${PREFIX}/share/man/man1" \
             -Dman3dir="${PREFIX}/share/man/man3" \
             -Dinstallsiteman1dir="${PREFIX}/share/man/man1" \
             -Dinstallsiteman3dir="${PREFIX}/share/man/man3" \
             -Dsiteman1dir="${PREFIX}/share/man/man1" \
             -Dsiteman3dir="${PREFIX}/share/man/man3" \
-            -Dotherlibdirs="${PREFIX}/lib/perl" \
             -A prepend:ldflags=" -headerpad_max_install_names -L${PREFIX}/lib " \
 	    -A prepend:lddlflags=" -headerpad_max_install_names -L${PREFIX}/lib " \
             -A prepend:cflags=" -I${PREFIX}/include " \
