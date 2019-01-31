327ac1c31003f20a0059e4b9099cd3ca207498df
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-01-31 15:09:44 -0500

    Update comment and fix error in DBD::Sybase post link

--- perl-dbd-sybase/post-link.sh
+++ perl-dbd-sybase/post-link.sh
@@ -5,6 +5,5 @@ if [ -e /soft/SYBASE15.7/OCS-15_0/lib ]
 then
     patchelf --set-rpath /soft/SYBASE15.7/OCS-15_0/lib ${PREFIX}/lib/site_perl/5.26.2/x86_64-linux/auto/DBD/Sybase/Sybase.so
 else
-    echo "Not installing perl-dbd-sybase. /soft/SYBASE15.7 not on this system"
-    eit 1
+    echo "Not patching rpath for perl-dbd-sybase. /soft/SYBASE15.7 not on this system"
 fi
