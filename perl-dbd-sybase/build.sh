6b277e4e280a1c4f6f6c11d4e224c9f6353fc1a1
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-07-24 14:42:13 -0400

    Update DBD::Sybase recipe

--- perl-dbd-sybase/build.sh
+++ perl-dbd-sybase/build.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-rm -f PWD.factory
-echo DB=aca_tstdb > PWD.factory
-echo SRV=sybase >> PWD.factory
-echo UID=aca_test >> PWD.factory
-echo PWD=`cat /proj/sot/ska/data/aspect_authorization/sybase-aca_tstdb-aca_test` >> PWD.factory
-chmod og-rwx PWD.factory
+#rm -f PWD.factory
+#echo DB=aca_tstdb > PWD.factory
+#echo SRV=sybase >> PWD.factory
+#echo UID=aca_test >> PWD.factory
+#echo PWD=`cat /proj/sot/ska/data/aspect_authorization/sybase-aca_tstdb-aca_test` >> PWD.factory
+#chmod og-rwx PWD.factory
 
 export SYBASE='/soft/SYBASE16.0'
 export SYBASE_OCS='OCS-16_0'
 export SYB_LIB_DIR=/soft/SYBASE16.0/OCS-16_0/lib
-#export LD_RUN_PATH=${SYBASE}/${SYBASE_OCS}/lib
-#perl -i -pe 's/$lddlflags = "-L/$lddlflags = "-Wl,-rpath,$ENV{SYB_LIB_DIR} -L/' Makefile.PL
-#rm -f t/xblk.t
+perl -i -pe 's/$lddlflags = "-L/$lddlflags = "-Wl,-rpath,$ENV{SYB_LIB_DIR} -L/' Makefile.PL
 
 perl Makefile.PL INSTALLDIRS=site --accept_test_defaults --chained Y --threaded_libs N
 make
-make test
+#make test
 make install
 
-#exit 1
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
