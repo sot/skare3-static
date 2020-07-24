dd85daf9990af66835961acdd93a990e0ac685e6
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-07-24 14:42:53 -0400

    Add a recipe for Perl Tk for aca_movie etc

---
+++ perl-tk/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Make sure this goes in site
+perl Makefile.PL INSTALLDIRS=site X11INC=${PREFIX}/include X11LIB=${PREFIX}/lib
+make
+#make test
+make install
+
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
