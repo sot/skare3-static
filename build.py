d01ffd759c3e3915678e386a3360a61fcdaf2867
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-07-28 09:41:38 -0400

    Use conda-forge Perl on Linux via the recipes

--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import argparse
+import platform
 
 parser = argparse.ArgumentParser(description="Build skare3 Perl packages.")
 
@@ -34,14 +35,28 @@ bonus_pkgs = [
 
 build_dir = os.path.join(args.build_root, "builds")
 
+system_name = platform.uname().system
+
 # If a package is requested, do that, else everything.
 if args.package is None:
     pkgs = req_pkgs
 elif args.package == 'all':
+    if system_name == 'Darwin':
+        print("Building all packages only supported on Linux")
+        exit(1)
     pkgs = req_pkgs + bonus_pkgs
 else:
     pkgs = [args.package]
 
 for pkg in pkgs:
-    subprocess.check_call(["conda", "build", "--use-local",
-                           "--perl", "5.26.2", "--croot", build_dir, pkg])
+    cmd = ["conda", "build", "-c", "defaults",
+           "--use-local",
+           "--perl", "5.26.2", "--croot", build_dir, pkg]
+
+    # Need conda-forge perl on Linux.  Would need to set up a local repo
+    # to enforce just that package.  Adding conda-forge after defaults seems
+    # to be working on Linux without taking that step (strict-channel-priority
+    # does not seem to be an option in conda-build).
+    if system_name == 'Linux':
+        cmd.extend(["-c", "conda-forge"])
+    subprocess.check_call(cmd)
