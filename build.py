f68f1e36098c6d385419c54cdd499b19d0bddaa1
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-12-27 12:21:15 -0500

    More tweaks

--- build.py
+++ build.py
@@ -16,41 +16,41 @@ parser.add_argument("--build-root", default=".", type=str,
                          "Default: '.' (makes 'builds' dir)")
 args = parser.parse_args()
 
-req_pkgs = []
+
 
 system_name = platform.uname().system
 machine = platform.uname().machine
 
 req_pkgs = []
 # Linux seems to need a built cpanminus for perl-extended-deps to work
-#if system_name == 'Linux':
-#    req_pkgs.append('perl-app-cpanminus')
+if system_name == 'Linux':
+    req_pkgs.append('perl-app-cpanminus')
 
 # perl-io-tty won't build on mac x86_64 but we want to build it on the other two
 # platforms
-#if (system_name == 'Linux') or ((system_name == 'Darwin') and (machine == 'arm64')):
-#    req_pkgs.append('perl-io-tty')
+if (system_name == 'Linux') or ((system_name == 'Darwin') and (machine == 'arm64')):
+    req_pkgs.append('perl-io-tty')
 
 # For all platforms, these two are needed
-#req_pkgs += ['perl-core-deps',
-#             'perl-ska-classic']
+req_pkgs += ['perl-core-deps',
+             'perl-ska-classic']
 
 bonus_pkgs = [
-#    'xtime',
-#    'perl-extended-deps',
-#    'perl-alien-cfitsio',
-#    'perl-astro-fits-cfitsio',
-#    'perl-astro-fits-header',
-#    'perl-astro-fits-cfitsio-checkstatus',
-#    'perl-pdl',
-#    'perl-astro-fits-cfitsio-simple',
-#    'perl-chandra-time',
-#    'perl-cxc-sysarch',
-#    'perl-app-env-ascds',
-#    'perl-ska-convert',
-#    'perl-ska-agasc',
-#    'perl-ska-web',
-#    'perl-dbd-sybase',
+    'xtime',
+    'perl-extended-deps',
+    'perl-alien-cfitsio',
+    'perl-astro-fits-cfitsio',
+    'perl-astro-fits-header',
+    'perl-astro-fits-cfitsio-checkstatus',
+    'perl-pdl',
+    'perl-astro-fits-cfitsio-simple',
+    'perl-chandra-time',
+    'perl-cxc-sysarch',
+    'perl-app-env-ascds',
+    'perl-ska-convert',
+    'perl-ska-agasc',
+    'perl-ska-web',
+    'perl-dbd-sybase',
     'perl-tk']
 
 
@@ -71,6 +71,7 @@ else:
 
 for pkg in pkgs:
     cmd = ["conda", "build"]
+    # These look to have some issue with prefix length
     if pkg in ['perl-astro-fits-header', 'perl-astro-fits-cfitsio-simple']:
         cmd.extend(["--prefix-length", "240"])
     cmd.extend(["--override-channels", "-c", "file://proj/sot/ska/www/ASPECT/ska3-conda/twelve",
